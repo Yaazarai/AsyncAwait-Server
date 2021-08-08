0058ff413deabb3ba708009b39701693659e4401
Alexander Griffis <fatalsleep@gmail.com>
2021-08-08 02:38:24 -0700

    ClientEventArgs removed, new ServerDataEventArgs

--- AsyncClient.cs
+++ AsyncClient.cs
@@ -28,12 +28,4 @@ namespace AsyncNetworking {
         public async Task SendAsync(byte[] buffer, int count = 0) =>
             await Client.GetStream().WriteAsync(buffer, 0, Math.Max(0, Math.Min(count, buffer.Length)));
     }
-
-    public class ClientEventArgs : EventArgs {
-        public object ClientObject { get; private set; }
-        public int PacketID { get; private set; }
-        public byte[] Buffer { get; private set; }
-        public ClientEventArgs(object client, byte[] buffer) { ClientObject = client; Buffer = buffer; }
-        public static new ClientEventArgs Empty { get { return new ClientEventArgs(null, null); } }
-    }
 }
